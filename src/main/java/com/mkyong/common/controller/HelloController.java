453b37827c01349e6e8e346793cf22140fd16082
donjorgitopt <58426149+donjorgitopt@users.noreply.github.com>
2019-12-02 14:28:50 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
  
 		model.addAttribute("message", "Spring Security Hello World");
-		return "Hola soy Don Jorgito 3";
+		return "Hola soy Don Jorgito 4";
  
 	}
  
